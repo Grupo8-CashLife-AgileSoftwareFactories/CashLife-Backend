177535dfa7a9c8f02710d5e86d0da9f61796cd7e
Andres <andresrojas0248@gmail.com>
2025-05-12 22:13:18 -0500

    add project backend CashLife

---
+++ src/main/java/com/cashbudget/cashlife/platform/CashLifeApplication.java
@@ -0,0 +1,13 @@
+package com.cashbudget.cashlife.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cashbudget/cashlife/platform/CashLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashbudget.cashlife.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
