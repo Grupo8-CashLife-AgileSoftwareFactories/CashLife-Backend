0807066d6e266b8c91dbd38a65a2e0eedf55cf49
Andrea O'Higgins <u20221b178@upc.edu.pe>
2025-07-13 00:20:43 -0500

    commit prueba cicd

--- src/main/java/com/cashbudget/cashlife/platform/CashLifeApplication.java
+++ src/main/java/com/cashbudget/cashlife/platform/CashLifeApplication.java
@@ -2,7 +2,7 @@ package com.cashbudget.cashlife.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// prueba
 @SpringBootApplication
 public class CashLifeApplication {
 
